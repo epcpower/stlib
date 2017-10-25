5b2b27703cf2f42d2691c2bddce037df420becbd
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-10-25 14:31:28 -0400

    Add tests.common.symbol_files

--- epyqlib/tests/common.py
+++ epyqlib/tests/common.py
@@ -25,6 +25,17 @@ devices = {
     for k, v in devices.items()
 }
 
+symbol_files = {
+    'customer': os.path.join('customer', 'EPC_DG_ID247.sym'),
+    'factory': os.path.join('factory', 'EPC_DG_ID247_FACTORY.sym'),
+}
+
+symbol_files = {
+    k: os.path.normpath(os.path.join(device_path, v))
+    for k, v in symbol_files.items()
+}
+
+
 @attr.s
 class Values:
     initial = attr.ib()
