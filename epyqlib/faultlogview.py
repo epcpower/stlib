03923097539d53c2dc4cfe9d6e2125de5890adc6
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-07-26 18:40:29 -0700

    Stop importing PyQt5.Qt

--- epyqlib/faultlogview.py
+++ epyqlib/faultlogview.py
@@ -1,7 +1,8 @@
 import io
 import os.path
 
-import PyQt5.Qt
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
 import PyQt5.uic
 
 # See file COPYING in this source tree
@@ -9,7 +10,7 @@ __copyright__ = 'Copyright 2018, EPC Power Corp.'
 __license__ = 'GPLv2+'
 
 
-class FaultLogView(PyQt5.Qt.QWidget):
+class FaultLogView(QtWidgets.QWidget):
     def __init__(self, parent=None, in_designer=False):
         super().__init__(parent=parent)
 
@@ -17,16 +18,16 @@ class FaultLogView(PyQt5.Qt.QWidget):
 
         ui = 'faultlogview.ui'
         # TODO: CAMPid 9549757292917394095482739548437597676742
-        if not PyQt5.Qt.QFileInfo(ui).isAbsolute():
+        if not QtCore.QFileInfo(ui).isAbsolute():
             ui_file = os.path.join(
-                PyQt5.Qt.QFileInfo.absolutePath(PyQt5.Qt.QFileInfo(__file__)),
+                QtCore.QFileInfo.absolutePath(QtCore.QFileInfo(__file__)),
                 ui,
             )
         else:
             ui_file = ui
-        ui_file = PyQt5.Qt.QFile(ui_file)
-        ui_file.open(PyQt5.Qt.QFile.ReadOnly | PyQt5.Qt.QFile.Text)
-        ts = PyQt5.Qt.QTextStream(ui_file)
+        ui_file = QtCore.QFile(ui_file)
+        ui_file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text)
+        ts = QtCore.QTextStream(ui_file)
         sio = io.StringIO(ts.readAll())
         self.ui = PyQt5.uic.loadUi(sio, self)
 
@@ -44,13 +45,13 @@ class FaultLogView(PyQt5.Qt.QWidget):
         self.ui.tree_view.setModel(model)
 
     def clear(self):
-        answer = PyQt5.Qt.QMessageBox.question(
+        answer = QtWidgets.QMessageBox.question(
             self,
             'Clear Log',
             'Are you sure you want to clear the log?',
         )
 
-        if answer != PyQt5.Qt.QMessageBox.Yes:
+        if answer != QtWidgets.QMessageBox.Yes:
             return
 
         children = list(self.model.root.children)
