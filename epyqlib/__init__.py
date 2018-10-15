d3b942f8b5766bece4742b611da233170007c6c0
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-10-15 15:39:23 -0400

    actually get __version__ and __sha__

--- epyqlib/__init__.py
+++ epyqlib/__init__.py
@@ -1,5 +1,12 @@
-from epyqlib._version import __version__, __sha__, __revision__
+import operator
+
 import epyqlib._build
+import epyqlib._version
+
+
+__version__, __sha__ = operator.itemgetter('version', 'full-revisionid')(
+    epyqlib._version.get_versions(),
+)
 
 __version_tag__ = 'v{}-{}'.format(__version__, __sha__)
 __build_tag__ = epyqlib._build.job_id
