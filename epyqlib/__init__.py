fe573bd8d2831c33bfd01045254fb2fb4c03d251
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-10-15 15:46:03 -0400

    more

--- epyqlib/__init__.py
+++ epyqlib/__init__.py
@@ -10,7 +10,3 @@ __version__, __sha__ = operator.itemgetter('version', 'full-revisionid')(
 
 __version_tag__ = 'v{}-{}'.format(__version__, __sha__)
 __build_tag__ = epyqlib._build.job_id
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
