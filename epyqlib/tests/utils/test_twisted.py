b34796cbba959d9cb2cb843f3cc5fc815c7cb6c6
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-10-26 09:06:11 -0400

    Tighten Twisted timing tests to 50ms

--- epyqlib/tests/utils/test_twisted.py
+++ epyqlib/tests/utils/test_twisted.py
@@ -44,7 +44,7 @@ class ActionLogger:
     def check(self):
         assert len(self.expected) == len(self.log)
         for result, expected in zip(self.log, self.expected):
-            assert abs(result.time - expected.time) < 0.125
+            assert abs(result.time - expected.time) < 0.050
             assert expected.data == result.data
 
 
