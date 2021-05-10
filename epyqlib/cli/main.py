7239412a9c4684349cd94d4ee673c89207cbf1e6
Gordon Schmidt <gordon.schmidt@epcpower.com>
2021-05-10 11:21:59 -0700

    feat (GT-135): phabricator extract unavailable exception handling

--- epyqlib/cli/main.py
+++ epyqlib/cli/main.py
@@ -1,7 +1,6 @@
 import click
 
 import epyqlib.cli.audit
-import epyqlib.cli.phabricator_extract
 import epyqlib.pm.valueset
 
 
@@ -12,6 +11,13 @@ def cli():
 
 cli.add_command(epyqlib.pm.valueset.group)
 cli.add_command(epyqlib.cli.audit.create_command(), name="audit")
-cli.add_command(
-    epyqlib.cli.phabricator_extract.create_command(), name="phabricator_extract"
-)
+try:
+    import epyqlib.cli.phabricator_extract
+
+    cli.add_command(
+        epyqlib.cli.phabricator_extract.create_command(), name="phabricator_extract"
+    )
+except ModuleNotFoundError as mnfe:
+    click.echo(
+        f"Phabricator extract functionality is unavailable due to missing module: {mnfe.name}"
+    )
