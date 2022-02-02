02dd584fba7299362105d452d470214a0263778e
Alex Anker <alex.anker@epcpower.com>
2022-02-02 15:00:08 -0800

    builduiepyqlib

--- buildui.py
+++
@@ -1,9 +0,0 @@
-import alqtendpy.compileui
-import pathlib
-
-
-def compile_ui():
-    print("epyqlib::compile_ui building UI in epyq")
-    alqtendpy.compileui.compile_ui(
-        directory_paths=[pathlib.Path(__file__).parent / "epyqlib"],
-    )
