7ec65366dfad74a2cfe27a3659236b5d5a130051
Gordon Schmidt <gordon.schmidt@epcpower.com>
2022-02-07 17:50:27 -0800

    feat (SC-399): poetry versioning

--- epyqlib/__init__.py
+++ epyqlib/__init__.py
@@ -1,12 +1,20 @@
-import operator
+# For development, the __version__ is set to the build placeholder 0.0.0
+# For release, the __version__ is the actual released version
+__version__ = "0.0.0"
 
-import epyqlib._build
-import epyqlib._version
+__sha__ = "SHA_to_be_removed?"
+__version_tag__ = "version_tag_to_be_removed?"
+__build_tag__ = "build_tag_to_be_removed?"
 
-
-__version__, __sha__ = operator.itemgetter("version", "full-revisionid")(
-    epyqlib._version.get_versions(),
-)
-
-__version_tag__ = "v{}-{}".format(__version__, __sha__)
-__build_tag__ = epyqlib._build.job_id
+# import operator
+#
+# import epyqlib._build
+# import epyqlib._version
+#
+#
+# __version__, __sha__ = operator.itemgetter("version", "full-revisionid")(
+#     epyqlib._version.get_versions(),
+# )
+#
+# __version_tag__ = "v{}-{}".format(__version__, __sha__)
+# __build_tag__ = epyqlib._build.job_id
