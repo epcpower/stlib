c26ba7b737cfe04cb5f1f91ac0929857cc3bb573
Gordon Schmidt <gordon.schmidt@epcpower.com>
2022-02-08 18:19:04 -0800

    feat (SC-399): version tag and build tag initialized

--- epyqlib/__init__.py
+++ epyqlib/__init__.py
@@ -2,19 +2,7 @@
 # For release, the __version__ is the actual released version
 __version__ = "0.0.0"
 
-__sha__ = "SHA_to_be_removed?"
-__version_tag__ = "version_tag_to_be_removed?"
-__build_tag__ = "build_tag_to_be_removed?"
+import epyqlib._build
 
-# import operator
-#
-# import epyqlib._build
-# import epyqlib._version
-#
-#
-# __version__, __sha__ = operator.itemgetter("version", "full-revisionid")(
-#     epyqlib._version.get_versions(),
-# )
-#
-# __version_tag__ = "v{}-{}".format(__version__, __sha__)
-# __build_tag__ = epyqlib._build.job_id
+__version_tag__ = "v{}".format(__version__)
+__build_tag__ = epyqlib._build.job_id
