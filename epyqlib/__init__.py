af06013c9b4246fba1fab72b34185a71c63e5392
Gordon Schmidt <gordon.schmidt@epcpower.com>
2022-02-17 11:26:56 -0800

    feat (SC-399): improve __version__ comments, remove commented poetry dynamic versioning substitution

--- epyqlib/__init__.py
+++ epyqlib/__init__.py
@@ -1,5 +1,5 @@
-# For development, the __version__ is set to the build placeholder 0.0.0
-# For release, the __version__ is the actual released version
+# For development and git commit, the __version__ is set to the build placeholder 0.0.0
+# For release, the __version__ is modified by poetry dynamic versioning with the actual released version
 __version__ = "0.0.0"
 
 import epyqlib._build
