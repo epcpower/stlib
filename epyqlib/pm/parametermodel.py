e812242d49349320a1dd78ea1436a21198606b79
Gordon Schmidt <gordon.schmidt@epcpower.com>
2026-05-22 14:22:46 -0700

    Revert "chore(SC-5421): Fix pre-existing test failures surfaced by CI"
    This reverts commit 67f9cf2524d8bb2e576cd8ccc2815a531bf00167.

--- epyqlib/pm/parametermodel.py
+++ epyqlib/pm/parametermodel.py
@@ -1574,7 +1574,6 @@ def merge(name, *types):
 
 columns = epyqlib.attrsmodel.columns(
     merge("name", *types.types.values()),
-    merge("abbreviation", SunSpecEnumerator),
     (merge("decoded_name", Parameter) + merge("type", SunSpecEnumerator)),
     merge("length", Array),
     merge(
